3309b9f965146e0c755626b79dc351a31f1faa7a
ArnoldPires <arnold.pires92@gmail.com>
2020-06-03 16:50:13 -0400

    created some js and added in goat sound files. Will have to replace them later

---
+++ js/main.js
@@ -0,0 +1,21 @@
+function toggle_visibility(id) {
+  let x = document.getElementById(id);
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+var bleepOne = new Audio();
+bleepOne.src = "sounds/maria.m4a";
+var bleepTwo = new Audio();
+bleepTwo.src = "sounds/hello.m4a";
+var bleepThree = new Audio();
+bleepThree.src = "sounds/testinggoat.m4a";
+var bleepFour = new Audio();
+bleepFour.src = "sounds/maria.m4a";
+var bleepFive = new Audio();
+bleepFive.src = "sounds/maria.m4a";
+var bleepSix = new Audio();
+bleepSix.src = "sounds/maria.m4a";
